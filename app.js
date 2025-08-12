c1d6940b71bc07e641c8f89eb2fef0fa382485c9
Callum Preston <CallumCPreston@outlook.com>
2025-08-12 12:36:20 +0100

    Create basic message board

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages});
+});
+
+app.get("/new", (req, res) => {
+  res.render("form")
+})
+
+app.post("/new", (req, res) => {
+  messages.push({ text: req.body.text, user: req.body.user, added: new Date() });
+  res.redirect("/");
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
